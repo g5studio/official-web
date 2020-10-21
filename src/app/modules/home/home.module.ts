652a80d020e8b1ddea42adeb958dd3d26fa8e832
William Chiang <william.chiang@tcghl.com>
2020-10-21 18:45:36 +0800

    feat(general-questions): create general questions

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -9,6 +9,7 @@ import { NewsComponent } from './components/news/news.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { OutcomeComponent } from './components/outcome/outcome.component';
 import { OutcomeCardComponent } from './components/outcome-card/outcome-card.component';
+import { GeneralQuestionsComponent } from './components/general-questions/general-questions.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { OutcomeCardComponent } from './components/outcome-card/outcome-card.com
     NewsComponent,
     AboutUsComponent,
     OutcomeComponent,
-    OutcomeCardComponent
+    OutcomeCardComponent,
+    GeneralQuestionsComponent
   ],
   imports: [
     CommonModule,
