d528738deccf7c7039986584430c707f3b18e2c0
William Chiang <william.chiang@tcghl.com>
2020-10-22 11:20:21 +0800

    feat(outcome): add view more button

--- src/app/modules/layout/components/menu/menu-list.ts
+++ src/app/modules/layout/components/menu/menu-list.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../../../environments/environment';
 import { IMenuItemParms } from '@utilities/interfaces/layout.interface';
 import { ToolTipPosition } from 'src/app/modules/shared/components/tooltip/tooltip.component';
 
@@ -5,17 +6,20 @@ export const MENU_LIST: IMenuItemParms[] = [
     {
         path: 'home',
         icon: 'dashboard',
-        i18n: 'Menu.Home'
+        i18n: 'Menu.Home',
+        hide: false
     },
     {
         path: 'guideline',
         icon: 'guide',
-        i18n: 'Menu.AboutUs'
+        i18n: 'Menu.AboutUs',
+        hide: environment.production
     },
     {
         path: 'training',
         icon: 'guidebook',
-        i18n: 'Menu.Training'
+        i18n: 'Menu.Training',
+        hide: environment.production
     }
     // {
     //     path: 'team',
