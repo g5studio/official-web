07c074e1494eba5cc3e29086208a5b92039346d3
William Chiang <william.chiang@tcghl.com>
2020-10-23 18:10:50 +0800

    feat(general-questions): add questions translate and contact info and email template

--- src/app/modules/home/components/general-questions/general-questions.component.ts
+++ src/app/modules/home/components/general-questions/general-questions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '@user/models/user.model';
+import { UserService } from '@user/services/user.service';
 
 @Component({
   selector: 'app-general-questions',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneralQuestionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public $user: UserService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public getEmailTemplate(user: User) {
+    return `mailto:hr.g5studio@gmail.com?subject=${user.profile.fullName}的私人問題&body=我已經看過官方網站的常見問題，仍無法解答，問題如下：%0D%0A%0D%0A`;
   }
 
 }
