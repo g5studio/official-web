51b8537610d2d52711b3c1a504e505aca2d114a1
William Chiang <william.chiang@tcghl.com>
2020-11-09 19:17:27 +0800

    feat(news): add news

--- src/app/modules/home/components/news/news.component.ts
+++ src/app/modules/home/components/news/news.component.ts
@@ -9,7 +9,29 @@ export class NewsComponent implements OnInit {
 
   constructor() { }
 
+  public current = 1;
+  public interval: any;
+
   ngOnInit(): void {
+    this.interval = setInterval(
+      _ => this.updateCurrent(),
+      2000
+    );
+  }
+
+  public mouseIn() {
+    clearInterval(this.interval);
+  }
+
+  public mouseLeave() {
+    this.interval = setInterval(
+      _ => this.updateCurrent(),
+      2000
+    );
+  }
+
+  public updateCurrent(current?: number) {
+    this.current = current || (this.current + 1 > 4 ? 1 : this.current + 1);
   }
 
 }
