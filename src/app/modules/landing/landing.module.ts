da2e387acc135756e7e3490bcd3212ce59c3067a
william.chiang <william.chiang@tcghl.com>
2021-03-15 01:24:42 +0800

    feature(carousel-controller): add carousel abstract

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './pages/landing/landing.component';
 import { SharedModule } from '@shared/shared.module';
